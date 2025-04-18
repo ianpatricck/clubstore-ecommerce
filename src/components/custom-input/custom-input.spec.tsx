0921117a0f072c1b19cce4441bc0d91ccc17db6b
ianpatricck <ianpatrick499@gmail.com>
2025-04-18 15:47:06 -0300

    test: add an 'expect' property to receive the value corretly

--- src/components/custom-input/custom-input.spec.tsx
+++ src/components/custom-input/custom-input.spec.tsx
@@ -1,38 +1,40 @@
-import { render } from '@testing-library/react'
-import CustomInput from './custom-input.component'
-import Colors from '../../theme/theme.color'
-import userEvent from '@testing-library/user-event'
+import { render } from "@testing-library/react";
+import CustomInput from "./custom-input.component";
+import Colors from "../../theme/theme.color";
+import userEvent from "@testing-library/user-event";
 
-describe('custom-input', () => {
-  it('should render with error if hasError is true', () => {
+describe("custom-input", () => {
+  it("should render with error if hasError is true", () => {
     const { getByPlaceholderText } = render(
-      <CustomInput placeholder="lorem ipsum" hasError />
-    )
+      <CustomInput placeholder="lorem ipsum" hasError />,
+    );
 
-    const input = getByPlaceholderText('lorem ipsum')
+    const input = getByPlaceholderText("lorem ipsum");
 
-    expect(input).toHaveStyle({ border: `2px solid ${Colors.error}` })
-  })
+    expect(input).toHaveStyle({ border: `2px solid ${Colors.error}` });
+  });
 
-  it('should render without error if hasError is false', () => {
+  it("should render without error if hasError is false", () => {
     const { getByPlaceholderText } = render(
-      <CustomInput placeholder="lorem ipsum" hasError={false} />
-    )
+      <CustomInput placeholder="lorem ipsum" hasError={false} />,
+    );
 
-    const input = getByPlaceholderText('lorem ipsum')
+    const input = getByPlaceholderText("lorem ipsum");
 
-    expect(input).toHaveStyle({ border: 'none' })
-  })
+    expect(input).toHaveStyle({ border: "none" });
+  });
 
-  it('should change value when user types', () => {
-    const { getByPlaceholderText, getByDisplayValue } = render(
-      <CustomInput placeholder="lorem ipsum" hasError={false} />
-    )
+  it("should change value when user types", async () => {
+    const user = userEvent.setup();
 
-    const input = getByPlaceholderText('lorem ipsum')
+    const { getByPlaceholderText } = render(
+      <CustomInput placeholder="lorem ipsum" hasError={false} />,
+    );
 
-    userEvent.type(input, 'Dolar sit')
+    const input = getByPlaceholderText("lorem ipsum");
 
-    getByDisplayValue('Dolar sit')
-  })
-})
+    await user.type(input, "Dolar sit");
+
+    expect(input).toHaveValue("Dolar sit");
+  });
+});
